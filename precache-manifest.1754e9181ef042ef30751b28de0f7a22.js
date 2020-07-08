ef54e05d2656dc128ca89740d398a9b4b3e978b3
E SATYA NIKHIL KRISHNA <coe17b021@iiitk.ac.in>
2020-07-08 13:03:55 +0530

    Updates

---
+++ precache-manifest.1754e9181ef042ef30751b28de0f7a22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57cadee0bded22eb073da3b4b824a2b",
+    "url": "/onboardservices/index.html"
+  },
+  {
+    "revision": "49df7307808b66f3e054",
+    "url": "/onboardservices/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e2163e53308ea3e00762",
+    "url": "/onboardservices/static/css/main.ac4f21d1.chunk.css"
+  },
+  {
+    "revision": "49df7307808b66f3e054",
+    "url": "/onboardservices/static/js/2.3a69be99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/onboardservices/static/js/2.3a69be99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2163e53308ea3e00762",
+    "url": "/onboardservices/static/js/main.4c34ca3f.chunk.js"
+  },
+  {
+    "revision": "5728c4ce552ec6611453",
+    "url": "/onboardservices/static/js/runtime-main.2b33a707.js"
+  },
+  {
+    "revision": "0904417a862b27323332b8069fa4c15b",
+    "url": "/onboardservices/static/media/1.0904417a.jpeg"
+  },
+  {
+    "revision": "77b8d6ecedd2b5d9291f0c88297564fb",
+    "url": "/onboardservices/static/media/5.77b8d6ec.jpeg"
+  }
+]);
